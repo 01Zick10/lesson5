08d886c0c98d4fdd09c46cdceb243f97bb3e14df
01Zick10 <93434230+01Zick10@users.noreply.github.com>
2021-11-10 18:51:49 +0500

    Update lesson5.py

--- lesson5.py
+++ lesson5.py
@@ -1,1 +1,28 @@
+#1
+set1 = {10, 20, 30, 40, 50}
+set2 = list(set1)
+for x in set1:
+    if x == 10 or x == 20 or x == 30:
+        set2.remove(x)
+set3 = set(set2)
+print(set3)
 
+
+#2
+list = {10, 20, 30, 40, 50}
+another_list = {60, 70, 80, 20, 10}
+list.intersection_update(another_list)
+print(sum(list))
+
+#3
+list = {10, 20, 30, 40, 50}
+another_list = {30, 40, 50, 60, 70}
+list.intersection_update(another_list)
+print(list)
+
+
+#4
+list = {10, 20, 30, 40, 50}
+another_list = {30, 40, 50, 60, 70}
+list.symmetric_difference_update(another_list)
+print(list)
